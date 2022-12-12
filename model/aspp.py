b28768e6c28d0f8a6678f1e6cd2ac969fd6c4006
Eric Wiener <ewiener@zoox.com>
2022-12-12 13:56:29 -0800

    Added comments to aspp

--- model/aspp.py
+++ model/aspp.py
@@ -83,17 +83,25 @@ class ASPP_Bottleneck(nn.Module):
         feature_map_h = feature_map.size()[2] # (== h/16)
         feature_map_w = feature_map.size()[3] # (== w/16)
 
+        # (a) one 1x1 convolution and three 3x3 convolutions with unit rates = (6, 12, 18)
         out_1x1 = F.relu(self.bn_conv_1x1_1(self.conv_1x1_1(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
         out_3x3_1 = F.relu(self.bn_conv_3x3_1(self.conv_3x3_1(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
         out_3x3_2 = F.relu(self.bn_conv_3x3_2(self.conv_3x3_2(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
         out_3x3_3 = F.relu(self.bn_conv_3x3_3(self.conv_3x3_3(feature_map))) # (shape: (batch_size, 256, h/16, w/16))
 
+        # (b) the image-level features
         out_img = self.avg_pool(feature_map) # (shape: (batch_size, 512, 1, 1))
         out_img = F.relu(self.bn_conv_1x1_2(self.conv_1x1_2(out_img))) # (shape: (batch_size, 256, 1, 1))
         out_img = F.upsample(out_img, size=(feature_map_h, feature_map_w), mode="bilinear") # (shape: (batch_size, 256, h/16, w/16))
 
+        # (c) the concatenation of (a) and (b)
         out = torch.cat([out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_img], 1) # (shape: (batch_size, 1280, h/16, w/16))
+        
+        # The resulting features from all the branches are then concatenated and
+        # pass through another 1 × 1 convolution (also with 256 filters and batch normalization)
         out = F.relu(self.bn_conv_1x1_3(self.conv_1x1_3(out))) # (shape: (batch_size, 256, h/16, w/16))
+        
+        # Final 1x1 convolution that generates the final logits
         out = self.conv_1x1_4(out) # (shape: (batch_size, num_classes, h/16, w/16))
 
         return out
